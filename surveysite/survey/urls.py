5169773cb1b90c2c62b42da210d677ca2d8b723c
Dutchman97 <makatipu97@hotmail.com>
2020-08-29 04:05:36 +0200

    Add basic form input options

--- surveysite/survey/urls.py
+++ surveysite/survey/urls.py
@@ -5,4 +5,5 @@ app_name = 'survey'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<year_season>/<pre_or_post>/', views.survey, name='survey'),
+    path('<year_season>/<pre_or_post>/submit/', views.submit, name='submit'),
 ]
--- surveysite/survey/views.py
+++ surveysite/survey/views.py
@@ -12,14 +12,7 @@ def index(request):
     return render(request, 'survey/index.html', context)
 
 def survey(request, year_season, pre_or_post):
-    if pre_or_post == 'pre':
-        is_preseason = True
-    elif pre_or_post == 'post':
-        is_preseason = False
-    else:
-        raise Http404("Survey does not exist!")
-
-    survey = get_object_or_404(Survey, year_season=year_season, is_preseason=is_preseason)
+    survey = get_survey_or_404(year_season, pre_or_post)
     if survey.is_ongoing:
         return form(request, survey)
     else:
@@ -36,3 +29,18 @@ def results(request, survey):
         'survey': survey,
     }
     return render(request, 'survey/results.html', context)
+
+def submit(request, year_season, pre_or_post):
+    survey = get_survey_or_404(year_season, pre_or_post)
+    return Http404()
+
+def get_survey_or_404(year_season, pre_or_post):
+    if pre_or_post == 'pre':
+        is_preseason = True
+    elif pre_or_post == 'post':
+        is_preseason = False
+    else:
+        raise Http404("Survey does not exist!")
+
+    survey = get_object_or_404(Survey, year_season=year_season, is_preseason=is_preseason)
+    return survey
