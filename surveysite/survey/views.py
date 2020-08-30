24f961ec41aa0e2a227d96d1079dd4f1a3258221
Dutchman97 <makatipu97@hotmail.com>
2020-08-30 18:08:32 +0200

    Get survey anime from Anime table instead of from Survey table
    anime row not yet removed from Survey table

--- surveysite/survey/views.py
+++ surveysite/survey/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404 # HttpResponse
 #from django.template import loader
-from .models import Survey
+from .models import Survey, Anime
 
+
+# ======= VIEWS =======
+# Index
 def index(request):
     survey_list = Survey.objects.order_by('year', 'season', 'is_preseason')
     context = {
@@ -11,6 +14,8 @@ def index(request):
 
     return render(request, 'survey/index.html', context)
 
+
+# Survey page, use form() if survey active, otherwise use results()
 def survey(request, year, season, pre_or_post):
     survey = get_survey_or_404(year, season, pre_or_post)
     if survey.is_ongoing:
@@ -21,6 +26,7 @@ def survey(request, year, season, pre_or_post):
 def form(request, survey):
     context = {
         'survey': survey,
+        'anime_list': get_survey_anime(survey),
     }
     return render(request, 'survey/form.html', context)
 
@@ -30,9 +36,24 @@ def results(request, survey):
     }
     return render(request, 'survey/results.html', context)
 
+
+
+# POST requests will be sent here
 def submit(request, year, season, pre_or_post):
     survey = get_survey_or_404(year, season, pre_or_post)
     return Http404()
+
+
+
+# ======= HELPER METHODS =======
+def get_survey_anime(survey):
+    anime_list = Anime.objects.filter(
+        start_year__lte=survey.year,
+        start_season__lte=survey.season,
+        end_year__gte=survey.year,
+        end_season__lte=survey.season,
+    )
+    return anime_list
 
 def get_survey_or_404(year, season, pre_or_post):
     if pre_or_post == 'pre':
