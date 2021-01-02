f9a95c9ff4f0a0723eeed16adf14bdd9783efebc
Dutchman97 <makatipu97@hotmail.com>
2021-01-02 17:41:05 +0100

    Further improve url patterns

--- survey/urls.py
+++ survey/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'survey'
+
+survey_patterns = [
+    path('<int:year>/<int:season>/<pre_or_post>/', views.form, name='form'),
+    path('<int:year>/<int:season>/<pre_or_post>/results/', views.results, name='results'),
+    path('<int:year>/<int:season>/<pre_or_post>/submit/', views.submit, name='submit'),
+]
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<year>/<season>/<pre_or_post>/', views.form, name='form'),
-    path('<year>/<season>/<pre_or_post>/results/', views.results, name='results'),
-    path('<year>/<season>/<pre_or_post>/submit/', views.submit, name='submit'),
+    path('survey/', include(survey_patterns))
 ]
