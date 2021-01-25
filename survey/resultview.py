46c7d31191883edc6ffe8154c3da417f2434780a
Dutchman97 <makatipu97@hotmail.com>
2021-01-25 23:14:49 +0100

    Remove old table classes/methods, improve results item id generation

--- survey/resultview.py
+++ survey/resultview.py
@@ -64,28 +64,10 @@ class ResultsView(BaseResultsView):
         survey = self._get_survey()
 
         results_generator = ResultsGenerator(survey)
-        # anime_series_data, special_anime_data = results_generator.get_anime_results_data()
 
-        # context['segment_list'] = [
-        #     {'title': table.title, 'table': table} if isinstance(table, ResultsTable) else {'title': table[0].title, 'table_list': table}
-        #     for table in self.__generate_table_list(anime_series_data, special_anime_data, survey.is_preseason)
-        # ]
 
-        # context['root_item'] = {
-        #     'type': 'segment',
-        #     'title': 'Anime Results',
-        #     'children': [{
-        #         'type': 'segment',
-        #         'title': 'Anime Series',
-        #         'children': [],
-        #     }, {
-        #         'type': 'segment',
-        #         'title': 'Anime OVAs/ONAs/Movies/Specials',
-        #         'children': [],
-        #     }],
-        # }
-
-        root_item = ResultsSegment('Anime Results', [
+        context['anime_info_json'], context['anime_series_data_json'], context['special_anime_data_json'] = results_generator.get_anime_results_data_json()
+        context['root_item'] = ResultsSegment(is_root=True, title='Anime Results', children=[
             ResultsSegment('Demographics', [
                 ResultsSegment('Popularity', [
                     ResultsTableWithTop3('Most Popular Anime Series', ResultsType.POPULARITY, top_count=10),
@@ -107,8 +89,6 @@ class ResultsView(BaseResultsView):
                 ResultsTableWithTop3('Most Anticipated Anime OVAs/ONAs/Movies/Specials' if survey.is_preseason else 'Best Anime OVAs/ONAs/Movies/Specials', ResultsType.SCORE, is_for_series=False, top_count=5),
             ]),
         ])
-        context['root_item'] = root_item
-        context['anime_info_json'], context['anime_series_data_json'], context['special_anime_data_json'] = results_generator.get_anime_results_data_json()
 
         survey_responses = Response.objects.filter(survey=survey)
         response_count = survey_responses.count()
@@ -140,71 +120,17 @@ class ResultsView(BaseResultsView):
         context['age_distribution'] = age_distribution
         context['age_distribution_max'] = age_max
         return context
-
-
-    def __generate_table_list(self, anime_series_data, special_anime_data, is_preseason):
-        popularity_table = ResultsTable('Most Popular Anime', anime_series_data)
-        popularity_table.generate(ResultsType.POPULARITY, row_count=10)
-
-        gender_popularity_ratio_table = ResultsTable('Biggest Gender Popularity Disparity', anime_series_data)
-        gender_popularity_ratio_table.generate(ResultsType.GENDER_POPULARITY_RATIO, ResultsType.POPULARITY, row_count=3)
-        gender_popularity_ratio_table_inv = ResultsTable('Biggest Gender Popularity Disparity', anime_series_data)
-        gender_popularity_ratio_table_inv.generate(ResultsType.GENDER_POPULARITY_RATIO_INV, ResultsType.POPULARITY, row_count=3)
-
-
-        score_table = ResultsTable('Most Anticipated Anime' if is_preseason else 'Best Anime of the Season', anime_series_data)
-        score_table.generate(ResultsType.SCORE, row_count=10)
-
-        gender_score_difference_table = ResultsTable('Biggest Gender Score Disparity', anime_series_data)
-        gender_score_difference_table.generate(ResultsType.GENDER_SCORE_DIFFERENCE, ResultsType.SCORE, row_count=3)
-        gender_score_difference_table_inv = ResultsTable('Biggest Gender Score Disparity', anime_series_data)
-        gender_score_difference_table_inv.generate(ResultsType.GENDER_SCORE_DIFFERENCE_INV, ResultsType.SCORE, row_count=3)
-        
-        special_popularity_table = ResultsTable('Most Popular Anime OVAs/ONAs/Movies/Specials', special_anime_data)
-        special_popularity_table.generate(ResultsType.POPULARITY, row_count=5)
-
-        age_table = ResultsTable('Average Age Per Viewer', anime_series_data)
-        age_table.generate(ResultsType.AGE, row_count=5)
-
-        if is_preseason:
-            return [
-                popularity_table, [gender_popularity_ratio_table, gender_popularity_ratio_table_inv],
-                score_table, [gender_score_difference_table, gender_score_difference_table_inv],
-                age_table,
-                special_popularity_table,
-            ]
-        else:
-            underwatched_table = ResultsTable('Most Underwatched Anime', anime_series_data)
-            underwatched_table.generate(ResultsType.UNDERWATCHED, ResultsType.POPULARITY, row_count=5)
-
-            surprise_table = ResultsTable('Most Surprising Anime', anime_series_data)
-            surprise_table.generate(ResultsType.SURPRISE, ResultsType.SCORE, row_count=5)
-
-            disappointment_table = ResultsTable('Most Disappointing Anime', anime_series_data)
-            disappointment_table.generate(ResultsType.DISAPPOINTMENT, ResultsType.SCORE, row_count=5)
-
-            special_score_table = ResultsTable('Most Anticipated Anime OVAs/ONAs/Movies/Specials' if is_preseason else 'Best Anime OVAs/ONAs/Movies/Specials', special_anime_data)
-            special_score_table.generate(ResultsType.SCORE, row_count=5)
-
-            return [
-                popularity_table, [gender_popularity_ratio_table, gender_popularity_ratio_table_inv], underwatched_table,
-                score_table, [gender_score_difference_table, gender_score_difference_table_inv],
-                surprise_table, disappointment_table,
-                age_table,
-                special_popularity_table, special_score_table,
-            ]
     
 
-RESULTS_ITEM_ID_COUNTER = 0
 class ResultsItem:
     def __init__(self, item_type, title):
-        global RESULTS_ITEM_ID_COUNTER
-        self.id = RESULTS_ITEM_ID_COUNTER
-        RESULTS_ITEM_ID_COUNTER += 1
-
         self.item_type = item_type
         self.title = title
     
+    def set_id(self, item_id):
+        self.id = item_id
+        return item_id + 1
+
     class ItemType(Enum):
         SEGMENT = auto()
         TABLE_WITH_TOP3 = auto()
@@ -212,13 +138,20 @@ class ResultsItem:
         SPLIT_TABLE = auto()
 
 class ResultsSegment(ResultsItem):
-    def __init__(self, title, children=[]):
+    def __init__(self, title, children=[], is_root=False):
         super().__init__(ResultsItem.ItemType.SEGMENT, title)
 
         self.children = children
 
-    def add_child(self, item):
-        self.children.append(item)
+        if is_root:
+            final_id = self.set_id(0)
+            print('Last id:', final_id)
+    
+    def set_id(self, start_item_id):
+        next_id = super().set_id(start_item_id)
+        for child in self.children:
+            next_id = child.set_id(next_id)
+        return next_id
 
 class ResultsTableWithTop3(ResultsItem):
     def __init__(self, title, main_result_type, bonus_result_type=None, is_for_series=True, top_count=None, bottom_count=None):
@@ -386,151 +319,6 @@ class ResultsGenerator:
 
         return adjusted_response_count
 
-
-
-class ResultsTable:
-    """Table with anime results."""
-
-    def __init__(self, title, anime_data, ignore_anime_below_threshold=True):
-        self.title = title
-        self.anime_data = anime_data
-        self.columns = []
-        self.data = {}
-
-        for anime in anime_data.keys():
-            if ignore_anime_below_threshold and anime_data[anime][ResultsType.POPULARITY] < ANIME_POPULARITY_THRESHOLD * 100:
-                continue
-            self.data[anime] = {}
-
-    def generate(self, datatype, extra_datatype=None, is_descending=True, row_count=0):
-        image_css_class = 'table-col-image-small' if datatype in [ResultsType.GENDER_POPULARITY_RATIO, ResultsType.GENDER_POPULARITY_RATIO_INV, ResultsType.GENDER_SCORE_DIFFERENCE, ResultsType.GENDER_SCORE_DIFFERENCE_INV] else 'table-col-image'
-        self.columns.append(ResultsTable.Column(
-            key='image',
-            label='',
-            td_class=image_css_class,
-            th_class=image_css_class,
-        ))
-
-        self.__add_name_column()
-        value_key = 'value'
-        extra_key = 'extra'
-        css_class = 'text-right table-col-other'
-
-        for anime in self.data.keys():
-            self.data[anime][value_key] = self.anime_data[anime][datatype]
-
-            if extra_datatype:
-                self.data[anime]['extra'] = self.anime_data[anime][extra_datatype]
-        
-        self.columns.append(ResultsTable.Column(
-            key=value_key,
-            label=datatype.value,
-            formatter=datatype.get_formatter_name(),
-            td_class=css_class,
-            th_class=css_class,
-        ))
-
-        if extra_datatype:
-            self.columns.append(ResultsTable.Column(
-                key=extra_key,
-                label=extra_datatype.value,
-                formatter=datatype.get_formatter_name(),
-                td_class=css_class,
-                th_class=css_class,
-            ))
-
-        self.__sort(value_key, is_descending, True)
-        if row_count:
-            self.__limit_rows(row_count)
-
-        self.__add_progressbar_width(datatype)
-
-
-    def __add_name_column(self):
-        key = 'name'
-        label = 'Anime'
-
-        for anime in self.data.keys():
-            self.data[anime][key] = AnimeUtil.get_name_list(anime)
-
-        css_class = 'table-col-name'
-        self.columns.append(ResultsTable.Column(
-            key=key,
-            label=label,
-            td_class=css_class,
-            th_class=css_class,
-        ))
-
-    def __add_rank_column(self):
-        i = 1
-        for _, row in self.data.items():
-            row['rank'] = i
-            i += 1
-        
-        css_class = 'table-col-index'
-        self.columns.insert(0, ResultsTable.Column(
-            key='rank',
-            label='',
-            td_class=css_class,
-            th_class=css_class,
-        ))
-
-    def __sort(self, key, is_descending, display_rank):
-        def get_sort_key(item):
-            value = item[1][key]
-            if value == float('inf') or value != value:
-                return float('-inf')
-            else:
-                return value
-
-        self.data = OrderedDict(sorted(
-            self.data.items(),
-            key=get_sort_key,
-            reverse=is_descending,
-        ))
-
-        if display_rank:
-            self.__add_rank_column()
-
-    def __limit_rows(self, row_count):
-        idx = 0
-        result = []
-        for key, value in self.data.items():
-            if idx == row_count:
-                break
-            result.append((key, value))
-            idx += 1
-        self.data = OrderedDict(result)
-
-    def __add_progressbar_width(self, datatype):
-        if datatype == ResultsType.SCORE:
-            max_value = 5.0
-            min_value = 3.0
-        elif datatype in [ResultsType.GENDER_SCORE_DIFFERENCE, ResultsType.GENDER_SCORE_DIFFERENCE_INV]:
-            max_value = 1.5
-            min_value = 0.0
-        elif datatype in [ResultsType.GENDER_POPULARITY_RATIO, ResultsType.GENDER_POPULARITY_RATIO_INV]:
-            max_value = 10.0
-            min_value = 0.0
-        else:
-            max_value = 85.0
-            min_value = 0.0
-
-        for anime in self.data.keys():
-            self.data[anime]['pbwidth'] = (min(self.data[anime]['value'], max_value) - min_value) / (max_value - min_value) * 100.0
-
-
-    class Column:
-        def __init__(self, key, label, formatter=None, td_class='', th_class=''):
-            self.key = key
-            self.label = label
-            self.formatter = formatter
-            self.tdClass = td_class
-            self.thClass = th_class
-        
-        def as_keyvalue(self):
-            attributes = inspect.getmembers(self, lambda member: not inspect.isroutine(member))
-            return [attribute for attribute in attributes if (attribute[1] or isinstance(attribute[1], str)) and not attribute[0].startswith('__')]
 
 
 class ResultsType(Enum):
