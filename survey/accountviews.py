abf312101cdd7ff439f31f3f73174799640c14fb
Dutchman97 <makatipu97@hotmail.com>
2021-04-25 18:08:08 +0200

    Convert logout view from func to class

--- survey/accountviews.py
+++ survey/accountviews.py
@@ -1,4 +1,5 @@
 from allauth.account.views import LoginView, LogoutView
+from django.views.generic import View
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.http import Http404
@@ -8,9 +9,11 @@ class SurveyLoginView(LoginView):
 
 survey_login_view = SurveyLoginView.as_view()
 
-def survey_logout_view(request):
-    if request.method == 'POST':
+class SurveyLogoutView(View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
         logout(request)
         return redirect(request.POST['next'])
-    else:
-        raise Http404()
+
+survey_logout_view = SurveyLogoutView.as_view()
