8615e56b35307aba2e0afb2d704d71787db335de
Dutchman97 <makatipu97@hotmail.com>
2021-06-09 18:20:09 +0200

    Display notifications using BootstrapVue toasts

--- survey/views/notifications.py
+++ survey/views/notifications.py
@@ -1,21 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from django.views.generic import View
 from survey.models import MissingAnime
 from survey.util import AnimeUtil
 
-@method_decorator([login_required], name='dispatch')
+@method_decorator([never_cache, login_required], name='dispatch')
 class NotificationsView(View):
     def get(self, request, *args, **kwargs):
         missinganime_queryset = MissingAnime.objects.filter(user=request.user, user_has_read=False, admin_has_reviewed=True)
         missinganime_queryset.update(user_has_read=True)
         
         response = {
-            'missing_anime': [{
-                    'submitted_name': missinganime.name,
-                    'submitted_link': missinganime.link,
-                    'name_list': AnimeUtil.get_name_list(missinganime.anime) if missinganime.anime else [],
+            'missingAnimeList': [{
+                    'submittedName': missinganime.name,
+                    'submittedLink': missinganime.link,
+                    'nameList': AnimeUtil.get_name_list(missinganime.anime) if missinganime.anime else [],
+                    'surveyName': str(missinganime.survey),
                 } for missinganime in missinganime_queryset
             ]
         }
