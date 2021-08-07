7f67c622aa4a21d3087df22e2f00580b7990a551
Dutchman97 <11137836+Dutchman97@users.noreply.github.com>
2021-08-07 02:56:25 +0200

    CORS stuff not needed anymore

--- surveysite/settings.py
+++ surveysite/settings.py
@@ -78,8 +78,6 @@ MIDDLEWARE = [
     'htmlmin.middleware.MarkRequestMiddleware',
 ]
 
-# TODO: Use environment variables or something for this
-CORS_ALLOWED_ORIGINS = ['http://localhost:8080', 'http://127.0.0.1:8080']
 ROOT_URLCONF = 'surveysite.urls'
 
 TEMPLATES = [
