07083b681c279d2011fc8b6803e76993c8ca45dd
Dutchman97 <11137836+Dutchman97@users.noreply.github.com>
2021-09-26 15:35:43 +0200

    Missing anime requests get saved now

--- survey/views/api/survey_missing_anime.py
+++ survey/views/api/survey_missing_anime.py
@@ -1,12 +1,16 @@
+from dataclasses import dataclass
+from django.core.exceptions import ValidationError
 from django.http.request import HttpRequest
 from django.http.response import JsonResponse
 from django.views.generic import View
 from http import HTTPStatus
 import json
-from survey.models import Survey
+import logging
+from survey.models import MissingAnime, Survey
+from survey.util.data import DataBase
 from survey.util.http import JsonErrorResponse
 from survey.util.survey import get_survey_or_404
-from typing import Any
+from typing import Any, Optional
 
 class SurveyMissingAnimeApi(View):
     def post(self, request: HttpRequest, *args, **kwargs):
@@ -24,4 +28,45 @@ class SurveyMissingAnimeApi(View):
         json_data: dict[str, dict[str, Any]] = json.loads(request.body)
         print(json_data)
 
-        return JsonResponse({})
+        try:
+            missing_anime_data = MissingAnimeData.from_dict(json_data)
+        except KeyError as e:
+            logging.error('An error occurred while parsing missing anime data: ' + str(e))
+            return JsonErrorResponse('Request data is invalid', HTTPStatus.BAD_REQUEST)
+
+        validation_errors = {}
+
+        missing_anime = missing_anime_data.to_model()
+        try:
+            missing_anime.full_clean()
+        except ValidationError as e:
+            print('Error:', e.message_dict)
+            validation_errors['missing_anime'] = e.message_dict
+
+        if validation_errors:
+            return JsonErrorResponse({'validation': validation_errors}, HTTPStatus.BAD_REQUEST)
+
+        missing_anime.survey = survey
+        missing_anime.user = request.user
+        missing_anime.save()
+
+        return JsonResponse({}, status=HTTPStatus.NO_CONTENT)
+
+@dataclass
+class MissingAnimeData(DataBase):
+    name: str
+    link: str
+    description: str
+
+    def to_model(self, model: Optional[MissingAnime]=None):
+        if model is None:
+            return MissingAnime(
+                name=self.name,
+                link=self.link,
+                description=self.description,
+            )
+        else:
+            model.name = self.name
+            model.link = self.link
+            model.description = self.description
+            return model
