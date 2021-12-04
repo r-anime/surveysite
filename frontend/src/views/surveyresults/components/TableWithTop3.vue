6f53833780794b7497a4498a49356b14c1af91a1
Dutchman97 <11137836+Dutchman97@users.noreply.github.com>
2021-12-05 00:57:49 +0100

    Add top 3

--- frontend/src/views/surveyresults/components/TableWithTop3.vue
+++ frontend/src/views/surveyresults/components/TableWithTop3.vue
@@ -1,9 +1,38 @@
 <template>
   <div class="row">
-    <div class="col col-4">
-      {{ resultTypes }}
+    <div class="col-md-4 col-8 text-center">
+      <div v-if="ranking.length > 0" class="row justify-content-center mb-4">
+        <div class="col-8 mb-2">
+          <AnimeImages :animeImages="ranking[0].anime.images"/>
+        </div>
+        <div class="col-12">
+          <AnimeNames :animeNames="ranking[0].anime.names" :showShortName="false"/>
+        </div>
+      </div>
+      <div v-if="ranking.length > 1" class="row justify-content-center text-smaller">
+        <div :class="{'col-6': ranking.length > 2, 'col': ranking.length === 2}">
+          <div class="row justify-content-center">
+            <div class="col-11 mb-1">
+              <AnimeImages :animeImages="ranking[1].anime.images"/>
+            </div>
+            <div class="col-12">
+              <AnimeNames :animeNames="ranking[1].anime.names" :showShortName="false"/>
+            </div>
+          </div>
+        </div>
+        <div v-if="ranking.length > 2" class="col-6">
+          <div class="row justify-content-center">
+            <div class="col-11 mb-1">
+              <AnimeImages :animeImages="ranking[2].anime.images"/>
+            </div>
+            <div class="col-12">
+              <AnimeNames :animeNames="ranking[2].anime.names" :showShortName="false"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col">
+    <div class="col-md col-12">
       <SimpleResultsTable :ranking="ranking" :resultTypes="resultTypes" :top="10"/>
     </div>
   </div>
@@ -12,17 +41,19 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import SimpleResultsTable from './SimpleResultsTable.vue';
+import AnimeImages from '@/components/AnimeImages.vue';
+import AnimeNames from '@/components/AnimeNames.vue';
 
 @Options({
   components: {
     SimpleResultsTable,
+    AnimeImages,
+    AnimeNames,
   },
   props: {
     ranking: Array, // { anime: AnimeData, result: number, extraResult: number }[]
     resultTypes: Array,
   },
 })
-export default class TableWithTop3 extends Vue {
-
-}
+export default class TableWithTop3 extends Vue {}
 </script>
