5c37f47881c1a2d6db314c817824b0d5f806c48d
Dutchman97 <11137836+Dutchman97@users.noreply.github.com>
2022-08-19 20:28:41 +0200

    Fix TheNotifications

--- frontend/src/components/TheNotifications.vue
+++ frontend/src/components/TheNotifications.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="toast-container mb-4">
-    <div v-for="(notification, idx) in notifications" :key="idx" class="toast align-items-center text-white border-0" :class="`bg-${notification.color}`" :id="`toast-${idx}`" role="alert" aria-live="assertive" aria-atomic="true">
+    <div v-for="(notification, idx) in notifications" :key="idx" class="toast align-items-center text-white border-0" :class="`bg-${notification.color}`" :id="`toast${idx}`" role="alert" aria-live="assertive" aria-atomic="true">
       <div class="d-flex">
         <div class="toast-body">
           {{ notification.message }}
@@ -14,20 +14,20 @@
 <script setup lang="ts">
 import NotificatonService, { type Notification } from '@/util/notification-service';
 import { Toast } from 'bootstrap';
-import { nextTick } from 'vue';
+import { nextTick, ref } from 'vue';
 
-const notifications: Notification[] = [];
+const notifications = ref<Notification[]>([]);
 
 
 NotificatonService.subscribe(addNotification);
 
 
 function addNotification(notification: Notification): void {
-  const idx = notifications.length;
-  notifications.push(notification);
+  const idx = notifications.value.length;
+  notifications.value.push(notification);
 
   nextTick(() => {
-    const toast = new Toast(`#toast-${idx}`, {
+    const toast = new Toast(`#toast${idx}`, {
       autohide: true,
       delay: 5000,
     });
