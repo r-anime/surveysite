c81658b4ce0dcac8b4b28aec1aa818c5162573c7
Dutchman97 <11137836+Dutchman97@users.noreply.github.com>
2022-08-20 19:33:47 +0200

    NotFound to composition API

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,11 +1,3 @@
 <template>
   Could not find {{ $route.path ? `"${$route.path}"` : 'that page' }}.
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
-})
-export default class NotFound extends Vue {}
-</script>
